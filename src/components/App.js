ce9d29ea0db1bec57bea538180c720880f827d81
Naveenkumar67 <naveenvempati4@gmail.com>
2021-02-23 22:12:12 +0530

    third

--- src/components/App.js
+++ src/components/App.js
@@ -39,7 +39,7 @@ const del=()=>{
 }
 
 const items=todos.map(function(todo){
-	return <li  className="list">{todo.value} (<a href="#">Edit</a> | <a href="#" onClick={()=>{delete_todo(todo.id)}}>Delete</a>)</li>
+	return <li  className="list">{todo.value} <button >Edit</button> | <button onClick={()=>{delete_todo(todo.id)}}>Delete</button></li>
 })
 
 
@@ -47,7 +47,8 @@ const items=todos.map(function(todo){
 	<div id="main" className="text-center">
 	<h1>Todo List</h1>
 	<textarea id="task" style={{maxWidth:300}} onChange={updatetodo}></textarea><br/>
-	<button id="btn"  onClick={()=>{task(todo)}}>Add</button>
+	<button id="btn"  onClick={()=>{task(todo)}}>Add</button><br/>
+
 	<div className="text-center">
 		<ul>
 			{items}
